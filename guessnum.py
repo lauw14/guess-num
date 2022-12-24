882bc03c7f7007aa5e27b00978e9cabd1ef474ac
lauw14 <lauw14@hotmail.com>
2022-12-24 19:13:22 +0800

    add count message

--- guessnum.py
+++ guessnum.py
@@ -1,14 +1,18 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('請猜1至100的其中一個數字： ')
 	num = int(num)
 	if num == r:
 		print('你猜中了')
+		print('這是你猜的第 ', count, '次')
 		break
 	elif num > r:
 		print('你的數字比答案大')
 	elif num < r:
 		print('你的數字比答案小')
-		
+	print('這是你猜的第 ', count, '次')
+
 	
